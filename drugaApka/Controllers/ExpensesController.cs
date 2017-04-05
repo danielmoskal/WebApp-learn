4dcc8a801d406c36b746482a0cfae570212d8e2a
Daniel Moskal <daniel19967@gmail.com>
2017-04-05 12:35:32 +0200

    main table of Expenses added

--- drugaApka/Controllers/ExpensesController.cs
+++ drugaApka/Controllers/ExpensesController.cs
@@ -12,17 +12,42 @@ namespace drugaApka.Controllers
         // GET: Expenses
         public ActionResult Index()
         {
+            List<Month> months = new List<Month>();
+            months.Add(new Month { MonthID = 1, MonthName = "Styczen" });
+            months.Add(new Month { MonthID = 2, MonthName = "Luty" });
+            months.Add(new Month { MonthID = 3, MonthName = "Marzec" });
+            months.Add(new Month { MonthID = 4, MonthName = "Kwiecien" });
+
             RentFlatModelContainer db = new RentFlatModelContainer();
             var expenses = db.EXPENSESSet;
-            //RentFlatModelContainer db = new RentFlatModelContainer();
-            //var expenses = db.EXPENSESSet;
-            //System.Data.Entity.DbSet<EXPENSES> currentExpense = null;
-            //if (expenses.Any(p => p.ExpensePerMonth == "April"))
-            //{
-            //    currentExpense.AddRange(expenses.Where(p => p.ExpensePerMonth == "April"));
-            //}
-
-            return View();
+            List<EXPENSES> exp = new List<EXPENSES>();
+            foreach (var x in months)
+            {
+                if (expenses.Any(p => p.ExpensePerMonth == x.MonthName))
+                {
+                    var currentExpense = expenses.RemoveRange(expenses.Where(p => p.ExpensePerMonth == x.MonthName));
+                    EXPENSES entity = new EXPENSES();
+                    foreach (var item in currentExpense)
+                    {
+                        if (entity.ExpensePerMonth == null && item.ExpensePerMonth != null)
+                            entity.ExpensePerMonth = item.ExpensePerMonth;
+                        if (entity.CurrentBill == null && item.CurrentBill != null)
+                            entity.CurrentBill = item.CurrentBill;
+                        if (entity.GasBill == null && item.GasBill != null)
+                            entity.GasBill = item.GasBill;
+                        if (entity.InternetBill == null && item.InternetBill != null)
+                            entity.InternetBill = item.InternetBill;
+                        if (entity.Rent == null && item.Rent != null)
+                            entity.Rent = item.Rent;
+                        if (entity.RentalFee == null && item.RentalFee != null)
+                            entity.RentalFee = item.RentalFee;
+                    }
+                    exp.Add(entity);
+                    //IEnumerable<EXPENSES> expp;
+                    //expp = exp;
+                }
+            }
+            return View(exp);
         }
 
         public ActionResult Details()
@@ -44,7 +69,10 @@ namespace drugaApka.Controllers
                 var searchExpenses = expenses.Where(p => p.ExpensePerMonth == miesiac || p.ExpenseDate.Equals(date));
                 return View("Details", searchExpenses);
             }
-            return View();
+            if (miesiac == "" && data == "")
+                return View("Details", expenses);
+            else
+                return View();
         }
     }
 }
---
+++ drugaApka/Models/Month.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace drugaApka.Models
+{
+    public class Month
+    {
+        public int MonthID { get; set; }
+        public string MonthName { get; set; }
+    }
+}
